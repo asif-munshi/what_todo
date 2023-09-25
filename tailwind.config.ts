e8f91a94627b94762f057eae172fcc0cdfd42d2a
asif-munshi <asifmunshe2978@gmail.com>
2023-09-25 20:08:11 +0600

    added screen size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
+    screens: {
+      xsm: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     container: {
       center: true,
       padding: "2rem",
