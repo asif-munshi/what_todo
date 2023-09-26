809ceac4b97afc731478dd97de6ca6742b1057b9
Asif Munshi <75932258+asif-munshi@users.noreply.github.com>
2023-09-26 23:46:04 +0600

    reddit_header_part1

---
+++ src/components/Header/MobileSidebar.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+
+export default function MobileSidebar() {
+  return (
+    <>
+      <Sheet>
+        <SheetTrigger asChild>
+          {/* Menu Button */}
+          <Button variant="ghost">
+            <Menu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent
+          side={"left"}
+          className="flex h-screen flex-col justify-between"
+        >
+          <SheetHeader>
+            <SheetTitle>What Todo</SheetTitle>
+            <SheetDescription>
+              Make changes to your profile here.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+      <Link href={"/"} className="text-lg font-bold">
+        WT
+      </Link>
+    </>
+  );
+}
