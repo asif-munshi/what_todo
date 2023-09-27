e2b72e6451d7906fb0b7eadedd78621ab5f6c8b6
Asif Munshi <75932258+asif-munshi@users.noreply.github.com>
2023-09-28 02:56:24 +0600

    initial_home_page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import SubHeader from "@/components/SubHeader/SubHeader";
+import CardTodo from "@/components/Cards/CardTodo";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#DAE0E6] pt-12">
       <div className="z-[3]">
         <div className="box-border flex max-w-full flex-row justify-center px-6 py-5">
-          <div className="w-full lg:w-[640px]">
+          <div className="flex w-full flex-col items-center justify-center gap-4">
             <SubHeader />
+            <div className="grid w-full grid-cols-1 place-items-center gap-[10px] xl:grid-cols-2 xl:gap-4">
+              <CardTodo />
+              <CardTodo />
+              <CardTodo />
+              <CardTodo />
+            </div>
           </div>
         </div>
       </div>
